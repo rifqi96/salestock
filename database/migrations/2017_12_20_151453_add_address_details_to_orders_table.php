519f03df8db1fe0270a79ba639b716d7ec5bf1e0
Rifqi <rifqi96@yahoo.com>
2017-12-20 22:19:49 +0700

    Adding address details to orders table migration

---
+++ database/migrations/2017_12_20_151453_add_address_details_to_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAddressDetailsToOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('name')->after('coupon_id');
+            $table->string('phone')->after('coupon_id');
+            $table->string('address')->after('coupon_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('name');
+            $table->dropColumn('phone');
+            $table->dropColumn('address');
+        });
+    }
+}
