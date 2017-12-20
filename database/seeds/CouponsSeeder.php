d046aabc79fd8d21f545b7aa716af273ce26bdc6
Rifqi <rifqi96@yahoo.com>
2017-12-20 12:59:13 +0700

    Fixing CouponsSeeder

--- database/seeds/CouponsSeeder.php
+++ database/seeds/CouponsSeeder.php
@@ -20,8 +20,8 @@ class CouponsSeeder extends Seeder
             'type' => 'percentage',
             'discount' => 30,
             'qty' => 10,
-            'start_date' => '2017-12-10',
-            'end_date' => '2017-12-30'
+            'start_at' => '2017-12-10',
+            'end_at' => '2017-12-30'
         ));
 
         array_push($data, array(
@@ -30,8 +30,8 @@ class CouponsSeeder extends Seeder
             'type' => 'nominal',
             'discount' => 50000,
             'qty' => 10,
-            'start_date' => '2017-12-15',
-            'end_date' => '2017-12-30'
+            'start_at' => '2017-12-15',
+            'end_at' => '2017-12-30'
         ));
 
         foreach($data as $key=>$val){
